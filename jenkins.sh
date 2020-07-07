5e36bb2434e4b210ff28240c4eea5e988996c52d
rgps4502 <55177355+rgps4502@users.noreply.github.com>
2020-07-07 23:13:01 +0800

    update jenkins.sh
    新增jenkins安裝腳本

---
+++ jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#安裝jenkins
+
+#java
+yum install java-1.8.0-openjdk -y
+
+原文網址：https://kknews.cc/tech/mpxpb46.html
+#jenkins穩定版
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins -y
+
+#設定啟動用戶以及 Port
+sed -i '29cJENKINS_USER="root"' /etc/sysconfig/jenkins
+sed -i '56cJENKINS_PORT="80"' /etc/sysconfig/jenkins
+
+#開機啟動
+sudo chkconfig jenkins on
+service jenkins start
+
+echo "將以下密碼貼入web password"
+password=sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+echo $password
